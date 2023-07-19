e89053c14514c2b965b5a410393e7d499afc584c
Jeremy Karagania <jeremykaragania@icloud.com>
2023-07-19 23:50:14 +0800

    Parse command-line arguments

--- pile/main.hs
+++ pile/main.hs
@@ -1,6 +1,24 @@
 module Main where
   import System.Environment
 
+  data Argument =
+    InFile String |
+    OutFile String deriving Show
+
+  parseArgs :: [String] -> [Argument] -> [Argument]
+
+  parseArgs [] args = args
+
+  parseArgs [x] args = do
+    parseArgs [] (InFile x : args)
+
+  parseArgs (x:xs) args = do
+    case x of
+      "-o" -> do
+        parseArgs (drop 1 xs) (OutFile (head xs) : args)
+      otherwise -> do
+        parseArgs xs (InFile x : args)
+
   main = do
     files <- getArgs
     return ()
