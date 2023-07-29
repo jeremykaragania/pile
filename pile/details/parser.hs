9dc80aedfd7bd848abc3b5e8afc18efbce8aa6cd
Jeremy Karagania <jeremykaragania@icloud.com>
2023-07-29 23:01:00 +0800

    Add more expressions

--- pile/details/parser.hs
+++ pile/details/parser.hs
@@ -1,8 +1,32 @@
 module Parser where
+  import Lexer
+
+  data Expr =
+    Primary PrimaryExpr |
+    Postfix PostfixExpr |
+    Unary UnaryExpr deriving Show
+
   data PrimaryExpr =
     Identifier String |
     FloatingConstant Double |
     IntegerConstant Integer|
     CharacterConstant Char|
     StringLiteral String |
-    ParensExpr PrimaryExpr deriving Show
+    ParensExpr Expr deriving Show
+
+  data PostfixExpr =
+    PrimaryExpr PrimaryExpr |
+    ArraySubscript PostfixExpr Expr |
+    StructMember PostfixExpr String |
+    UnionMember PostfixExpr String |
+    PostfixIncrement PostfixExpr |
+    PostfixDecrement PostfixExpr deriving Show
+
+  data UnaryExpr =
+    PostfixExpr PostfixExpr |
+    PrefixIncrement UnaryExpr |
+    PrefixDecrement UnaryExpr |
+    AddressOperator UnaryExpr |
+    IndirectionOperator UnaryExpr |
+    ArithmeticOperator UnaryExpr |
+    SizeofOperator (Either UnaryExpr String) deriving Show
