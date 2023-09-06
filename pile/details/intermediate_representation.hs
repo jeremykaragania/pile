c3012b578b313c451c0c5917600d37faba305d14
Jeremy Karagania <jeremykaragania@icloud.com>
2023-09-06 22:16:13 +0800

    Add instructions

--- pile/details/intermediate_representation.hs
+++ pile/details/intermediate_representation.hs
@@ -2,7 +2,7 @@ module IntermediateRepresentation where
 
   data IRType =
     IRVoid |
-    IRFunction IRType [IRType] |
+    IRFunction Bool IRType [IRType] |
     IRInteger Integer |
     IRFloat Double |
     IRDouble Double |
@@ -18,9 +18,34 @@ module IntermediateRepresentation where
     IRArrayConstant IRType |
     IRStructureConstant IRType deriving Show
     
-  data IRInstruction = IRInstruction deriving Show
+  data IRInstruction =
+    IRRet (Maybe IRArgument) |
+    IRBrConditional IRArgument String String |
+    IRBRUnconditional String |
+    IRSwitch IRArgument String [(IRConstant, String)] |
+    IRFneg IRArgument |
+    IRAdd IRArgument IRArgument |
+    IRFadd IRArgument IRArgument |
+    IRSub IRArgument IRArgument |
+    IRMul IRArgument IRArgument |
+    IRFMul IRArgument IRArgument |
+    IRUdiv IRArgument IRArgument |
+    IRSdiv IRArgument IRArgument |
+    IRFdiv IRArgument IRArgument |
+    IRUrem IRArgument IRArgument |
+    IRSrem IRArgument IRArgument |
+    IRFrem IRArgument IRArgument |
+    IRShl IRArgument IRArgument |
+    IRLshr IRArgument IRArgument |
+    IRAshr IRArgument IRArgument |
+    IRAnd IRArgument IRArgument |
+    IROr IRArgument IRArgument |
+    IRXOr IRArgument IRArgument |
+    IRAlloca IRType (Maybe IRArgument) (Maybe Integer) |
+    IRLoad IRArgument (Maybe Integer) |
+    IRStore IRArgument IRArgument (Maybe Integer) deriving Show
 
-  data IRBasicBlock = IRBasicBlock [IRInstruction] deriving Show
+  data IRBasicBlock = IRBasicBlock [((Maybe String), IRInstruction)] deriving Show
 
   data IRArgument = IRArgument IRType (Maybe String) deriving Show
 
