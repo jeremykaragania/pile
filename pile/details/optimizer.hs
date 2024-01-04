54e48d5754bf61e8f6e0eba751dc3547519b111c
Jeremy Karagania <jeremykaragania@icloud.com>
2024-01-04 21:15:17 +0300

    Update optimizer

--- pile/details/optimizer.hs
+++ pile/details/optimizer.hs
@@ -3,6 +3,9 @@ module Optimizer where
   import Selector
   import Syntax
 
+  subStackPointer (MCInstruction (OpcodeCondition ARMSub _) [_, Scheduler.Register 13, _]) = True
+  subStackPointer _ = False
+
   replaceMachineCodes a [] = a
 
   replaceMachineCodes a (b@(MCInstruction (OpcodeCondition ARMStr _) [c, d, e]):f@(MCInstruction (OpcodeCondition ARMLdr _) [g, h, i]):j)
@@ -11,4 +14,9 @@ module Optimizer where
 
   replaceMachineCodes a (b:c) = replaceMachineCodes (a ++ [b]) c
 
-  optimizeMachineCodes = map (replaceMachineCodes [])
+  optimizeStackPointers a = [MCInstruction (OpcodeCondition ARMSub Nothing) [Scheduler.Register 13, Scheduler.Register 13, Immediate offset]] ++ (filter (not . subStackPointer) a)
+    where
+      immediateValue (MCInstruction (OpcodeCondition _ _) [_, _, Immediate a]) = a
+      offset = (sum . map immediateValue . filter subStackPointer) a
+
+  optimizeMachineCodes = map (optimizeStackPointers . replaceMachineCodes [])
